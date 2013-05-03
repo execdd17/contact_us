d9d9eaa7e4d5b6d396f5156fa80af3c337b45463
Alexander Vanadio <avanadio@gmail.com>
2013-05-02 23:56:01 -0400

    Added support for humanizer

--- app/models/contact_us/contact.rb
+++ app/models/contact_us/contact.rb
@@ -2,13 +2,22 @@ class ContactUs::Contact
   include ActiveModel::Conversion
   include ActiveModel::Validations
 
+  include Humanizer       
+  require_human_on :create
+
   attr_accessor :email, :message, :name, :subject
+  attr_accessor :humanizer_answer, :humanizer_question_id
 
   validates :email,   :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i },
                       :presence => true
   validates :message, :presence => true
   validates :name,    :presence => {:if => Proc.new{ContactUs.require_name}}
   validates :subject, :presence => {:if => Proc.new{ContactUs.require_subject}}
+  validate :is_human?
+
+  def is_human?
+    errors[:humanizer_answer] = 'Incorrect Answer' unless humanizer_correct_answer?
+  end
 
   def initialize(attributes = {})
     attributes.each do |key, value|
@@ -27,5 +36,4 @@ class ContactUs::Contact
   def persisted?
     false
   end
-
 end
